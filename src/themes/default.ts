cbdc942b3c762bedef7b2fbefa21734f8b19b024
IsaacDSC <isaac8.silva@hotmail.com>
2024-01-14 08:34:42 -0300

    initial commit

---
+++ src/themes/default.ts
@@ -0,0 +1,154 @@
+export const Themes = {
+  light: {
+    text: {
+      h1: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h2: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h3: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h4: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h5: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      p: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      small: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      }
+    },
+    background: {
+      one: {
+        color: "string",
+      },
+      tow: {
+        color: "string",
+      }
+    },
+    buttons: {
+      emphasis: {
+        color: "string",
+        backgroundColor: "string",
+        borderColor: "string",
+        hover: {
+          color: "string",
+          backgroundColor: "string",
+          borderColor: "string",
+        }
+      },
+      basic: {
+        color: "string",
+        backgroundColor: "string",
+        borderColor: "string",
+        hover: {
+          color: "string",
+          backgroundColor: "string",
+          borderColor: "string",
+        }
+      }
+    },
+  },
+  dark: {
+    text: {
+      h1: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h2: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h3: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h4: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      h5: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      p: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      },
+      small: {
+        color: "string",
+        hover: {
+          color: "string",
+        }
+      }
+    },
+    background: {
+      one: {
+        color: "string",
+      },
+      tow: {
+        color: "string",
+      }
+    },
+    buttons: {
+      emphasis: {
+        color: "string",
+        backgroundColor: "string",
+        borderColor: "string",
+        hover: {
+          color: "string",
+          backgroundColor: "string",
+          borderColor: "string",
+        }
+      },
+      basic: {
+        color: "string",
+        backgroundColor: "string",
+        borderColor: "string",
+        hover: {
+          color: "string",
+          backgroundColor: "string",
+          borderColor: "string",
+        }
+      }
+    },
+  },
+}
